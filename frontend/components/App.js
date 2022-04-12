df2d959c9b045b6d7ede8ded6a2be9430eed4138
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-12 15:42:21 -0700

    app skeleton setup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,29 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div className="app">
+          <h1>To-Do List</h1>
+
+          <div className="tasks">
+            <h2>Tasks: </h2>
+            <ul>
+              <li> module 2 project </li>
+              <li> have dinner </li>
+            </ul>
+          </div>
+          <form>
+            <input
+              placeholder="type to-do"
+            />
+            <button>add</button>
+          </form>
+
+          <button>clear</button>
+
+
+
+      </div>
+    )
   }
 }
